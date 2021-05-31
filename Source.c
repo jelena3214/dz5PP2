2f08dc53fa6d9a662a2c7c73e4b2c9f3fed359ca
Јелена Цветић <cj200305d@student.etf.bg.ac.rs>
2021-05-31 22:57:28 +0200

    memori lik

--- Source.c
+++ Source.c
@@ -1,4 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -46,6 +48,10 @@ Nodeinfo *readInput(Nodeinfo* info, FILE *in) {
 
 struct linkedListNode* createNode(Nodeinfo *info) {
 	struct linkedListNode* newnode = malloc(sizeof(struct linkedListNode));
+	if (!newnode) {
+		printf("MEM_GRESKA");
+		exit(0);
+	}
 	newnode->info = info;
 	newnode->next = NULL;
 	newnode->prev = NULL;
@@ -53,10 +59,18 @@ struct linkedListNode* createNode(Nodeinfo *info) {
 
 struct linkedlistNode* createLinkedList(char *filename) {
 	FILE* in = fopen(filename, "r");
+	if (!in) {
+		printf("DAT_GRESKA");
+		exit(0);
+	}
 	struct linkedListNode* head = NULL;
 	struct linkedListNode* p = NULL;
 	while (1) {
 		Nodeinfo* info = malloc(sizeof(Nodeinfo));
+		if (!info) {
+			printf("MEM_GRESKA");
+			exit(0);
+		}
 		info = readInput(info, in);
 		if (info == NULL)break;
 
@@ -103,6 +117,10 @@ void printLinkedlist(struct linkedListNode* head) {
 
 void writetoFile(char *filename, struct linkedListNode *begin) {
 	FILE* write = fopen(filename, "w");
+	if (!write) {
+		printf("DAT_GRESKA");
+		exit(0);
+	}
 	double Alldistance = 0.0, temp;
 	struct linkedListNode* p = begin;
 	struct linkedListNode* s = p->next;
@@ -113,13 +131,13 @@ void writetoFile(char *filename, struct linkedListNode *begin) {
 		p = p->next;
 		s = p->next;
 	}
-	fprintf(write, "%.2lf", Alldistance);
+	fprintf(write, "%.2lf\n", Alldistance);
 	fclose(write);
 }
 
 void freeList(struct linkedListNode* head) {
 	struct linkedListNode* p = head;
-	struct linkedListNode* temp = head;
+	struct linkedListNode* temp;
 	while (p != NULL) {
 		temp = p;
 		p = p->next;
@@ -130,68 +148,41 @@ void freeList(struct linkedListNode* head) {
 //free
 //2_dirA_distance.txt 5_dirA_distance.txt
 //dirA 2 5 7 10
-int main() {
-	char* name = malloc(sizeof(char) * 5);
-	scanf("%s", name);
-	char k = getchar();
-
-	char *number = malloc(sizeof(char));
-	char* filename = NULL;
-	char temp;
-	int i = 0, numSize = 1;
-	char* outfile = NULL;
-
-	while ((temp = getchar()) != '\n') {
-		if (temp == ' ') {
-			char* filename = malloc(sizeof(char) * 50);
-			char* outfile = malloc(sizeof(char) * 50);
-			filename[0] = '\0';
-			outfile[0] = '\0';
-			strcat(filename, number);
-			strcat(filename, "_\0");
-			strcat(filename, name);
-			strcat(filename, ".txt");
-			strcat(outfile, number);
-			strcat(outfile, "_\0");
-			strcat(outfile, name);
-			strcat(outfile, "_distance.txt");
-
-			struct linkedListNode* begin = createLinkedList(filename);
-			writetoFile(outfile, begin);
-
-			freeList(begin);
-			free(outfile);
-			free(filename);
-			free(number);
-			number = malloc(sizeof(char));
-			numSize = 1;
-			continue;
-		}
-		number = realloc(number, sizeof(char) * (numSize + 1));
-		number[numSize - 1] = temp;
-		number[numSize++] = '\0';
+int main(int argc, const char **argv[]) {
+	char* name = argv[1];
+	if (argc <= 2) {
+		printf("ARG_GRESKA");
+		exit(0);
 	}
+	for (int i = 2; i < argc; i++) {
+		char* filename = malloc(sizeof(char) * 50);
+		if (!filename) {
+			printf("MEM_GRESKA");
+			exit(0);
+		}
+		char* outfile = malloc(sizeof(char) * 50);
+		if (!outfile) {
+			printf("MEM_GRESKA");
+			exit(0);
+		}
+		filename[0] = '\0';
+		outfile[0] = '\0';
+		strcat(filename, argv[i]);
+		strcat(filename, "_\0");
+		strcat(filename, name);
+		strcat(filename, ".txt");
+		strcat(outfile, argv[i]);
+		strcat(outfile, "_\0");
+		strcat(outfile, name);
+		strcat(outfile, "_distance.txt");
 
-	filename = malloc(sizeof(char) * 50);
-	outfile = malloc(sizeof(char) * 50);
-	filename[0] = '\0';
-	outfile[0] = '\0';
-	strcat(filename, number);
-	strcat(filename, "_\0");
-	strcat(filename, name);
-	strcat(filename, ".txt");
-	strcat(outfile, number);
-	strcat(outfile, "_\0");
-	strcat(outfile, name);
-	strcat(outfile, "_distance.txt");
+		struct linkedListNode* begin = createLinkedList(filename);
+		writetoFile(outfile, begin);
 
-	struct linkedListNode* begin = createLinkedList(filename);
-	writetoFile(outfile, begin);
-
-	freeList(begin);
-	free(outfile);
-	free(filename);
-	free(number);
-
+		freeList(begin);
+		free(outfile);
+		free(filename);
+	}
+	_CrtDumpMemoryLeaks();
 	return 0;
 }
